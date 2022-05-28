ae0949f742636efd09e228a9f4a7c1d73e3fc5db
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-05-28 23:08:19 +0530

    python-still-sucks

--- plot.jl
+++ plot.jl
@@ -25,23 +25,23 @@ plt_curves = plot(curve_fittig_result["x"],y,label=labels)
 
 savefig("plt_curves.png")
 
-# y = [
-#     curve_fittig_result["err_2"],
-#     curve_fittig_result["err_3"],
-#     curve_fittig_result["err_4"], 
-#     curve_fittig_result["err_5"],
-#     curve_fittig_result["err_6"],
-#     curve_fittig_result["err_7"],
-#     curve_fittig_result["err_8"],
-#     curve_fittig_result["err_9"],
-#     curve_fittig_result["err_10"],
-# ]
+y = [
+    curve_fittig_result["err_2"],
+    curve_fittig_result["err_3"],
+    curve_fittig_result["err_4"], 
+    curve_fittig_result["err_5"],
+    curve_fittig_result["err_6"],
+    curve_fittig_result["err_7"],
+    curve_fittig_result["err_8"],
+    curve_fittig_result["err_9"],
+    curve_fittig_result["err_10"],
+]
 
 
 
-# labels = ["y" "err 2" "err 3" "err 4" "err 5" "err 6" "err 7" "err 8" "err 9" "err 10"]
+labels = ["y" "err 2" "err 3" "err 4" "err 5" "err 6" "err 7" "err 8" "err 9" "err 10"]
 
 
-# plt_curves = plot(curve_fittig_result["x"],y,label=labels)
+plt_curves = plot(curve_fittig_result["x"],y,label=labels)
 
-# savefig("plt_err_curves.png")
+savefig("plt_err_curves.png")
