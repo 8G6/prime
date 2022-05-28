b4deeb3d76568d8145fe4aa32b356f0194cf8028
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-05-28 23:07:57 +0530

    python-sucks

--- plot.jl
+++ plot.jl
@@ -18,30 +18,30 @@ y = [
 
 
 
-labels = ["y" "degree 2" "degree 3" "degree 4" "degree 5" "degree 6" "degree 7" "degree 8" "degree 9" "degree 10"]
+labels = ["Actual Curve" "degree 2" "degree 3" "degree 4" "degree 5" "degree 6" "degree 7" "degree 8" "degree 9" "degree 10"]
 
 plt_curves = plot(curve_fittig_result["x"],y,label=labels)
 
 
 savefig("plt_curves.png")
 
-y = [
-    curve_fittig_result["err_2"],
-    curve_fittig_result["err_3"],
-    curve_fittig_result["err_4"], 
-    curve_fittig_result["err_5"],
-    curve_fittig_result["err_6"],
-    curve_fittig_result["err_7"],
-    curve_fittig_result["err_8"],
-    curve_fittig_result["err_9"],
-    curve_fittig_result["err_10"],
-]
+# y = [
+#     curve_fittig_result["err_2"],
+#     curve_fittig_result["err_3"],
+#     curve_fittig_result["err_4"], 
+#     curve_fittig_result["err_5"],
+#     curve_fittig_result["err_6"],
+#     curve_fittig_result["err_7"],
+#     curve_fittig_result["err_8"],
+#     curve_fittig_result["err_9"],
+#     curve_fittig_result["err_10"],
+# ]
 
 
 
-labels = ["y" "err 2" "err 3" "err 4" "err 5" "err 6" "err 7" "err 8" "err 9" "err 10"]
+# labels = ["y" "err 2" "err 3" "err 4" "err 5" "err 6" "err 7" "err 8" "err 9" "err 10"]
 
 
-plt_curves = plot(curve_fittig_result["x"],y,label=labels)
+# plt_curves = plot(curve_fittig_result["x"],y,label=labels)
 
-savefig("plt_err_curves.png")
+# savefig("plt_err_curves.png")
